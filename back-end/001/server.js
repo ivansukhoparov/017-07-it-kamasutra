56ff2de8940fbaa039a08595b1cd7308f3262e23
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-29 12:01:07 +0100

    refraction: delete unuseble code and create function for creating messages

--- back-end/001/server.js
+++ back-end/001/server.js
@@ -2,38 +2,36 @@ const http = require('http');
 const fs = require('fs');
 
 let requestCounter = 0;
-const testFilePath = "index.html";
+
 
 const server = http.createServer((request, response) => {
-    if (request.url != '/favicon.ico') {
-        requestCounter++;
-    }
-
     switch (request.url) {
         case '/':
         case '/hello':
-            response.write('Hello, world! This is reques number: ' + requestCounter);
-            response.end();
+            response.end(messasge('Hello, world!'));
             break;
         case '/about':
-            response.write('It is about page. This is reques number: ' + requestCounter);
-            response.end();
+            response.end(messasge('It is about page.'));
             break;
         case '/payment':
-            response.write('Hahaha! All your money is mine now!. This is reques number: ' + requestCounter);
-            response.end();
+            response.end(messasge('Hahaha! All your money is mine now!'));
             break;
         default:
             const filePath = request.url.substring(1);
             fs.readFile(filePath, (err, data) => {
                 if (err) {
                     response.statusCode = 404;
-                    response.end('404 - Resourse not found. This is reques number: ' + requestCounter);
+                    response.end('404 - Resourse not found.');
                 } else {
                     response.end(data)
                 }
             })
     }
 })
+
+let messasge = function (msg) {
+    requestCounter++;
+    return msg + ' This is reques number: ' + requestCounter
+}
 
 server.listen(3003)
