539f23d0f0a42ad0c4216d8ec3a8a306b93bccbd
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-29 20:06:30 +0100

    create: create async function for reading files

--- back-end/002/server.js
+++ back-end/002/server.js
@@ -1,6 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
+const readFile = function (path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        })
+    })
+}
+
 const server = http.createServer((request, response) => {
     switch (request.url) {
         case '/':
