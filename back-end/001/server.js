97d7277cc050da9d0f90c359ae08e40bfaeffb19
ivansukhoparov <rbcdaaa@gmail.com>
2023-10-29 11:38:41 +0100

    create: create handle for processing file access request / add: add test files

--- back-end/001/server.js
+++ back-end/001/server.js
@@ -1,29 +1,39 @@
 const http = require('http');
+const fs = require('fs');
 
 let requestCounter = 0;
+const testFilePath = "index.html";
 
 const server = http.createServer((request, response) => {
     if (request.url != '/favicon.ico') {
         requestCounter++;
     }
 
-
     switch (request.url) {
         case '/':
         case '/hello':
-            response.write('Hello, world! This is reques number: ' + requestCounter)
+            response.write('Hello, world! This is reques number: ' + requestCounter);
+            response.end();
             break;
         case '/about':
-            response.write('It is about page. This is reques number: ' + requestCounter)
+            response.write('It is about page. This is reques number: ' + requestCounter);
+            response.end();
             break;
         case '/payment':
-            response.write('Hahaha! All your money is mine now!. This is reques number: ' + requestCounter)
+            response.write('Hahaha! All your money is mine now!. This is reques number: ' + requestCounter);
+            response.end();
             break;
         default:
-            response.write('404 page not found. This is reques number: ' + requestCounter)
+            const filePath = request.url.substring(1);
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    response.statusCode = 404;
+                    response.end('404 - Resourse not found. This is reques number: ' + requestCounter);
+                } else {
+                    response.end(data)
+                }
+            })
     }
-
-    response.end()
 })
 
 server.listen(3003)
