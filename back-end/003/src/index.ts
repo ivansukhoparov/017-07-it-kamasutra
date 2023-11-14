190539ad28661f4f9477e976e94103ee8a0ce783
ivansukhoparov <rbcdaaa@gmail.com>
2023-11-14 23:52:55 +0100

    create: create get, post and delete requests

--- back-end/003/src/index.ts
+++ back-end/003/src/index.ts
@@ -2,10 +2,70 @@ import express, {Request,Response} from "express";
 
 const app = express();
 const port :5000 = 5000;
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
 
-app.get("/", (req:Request,res:Response)=>{
-    res.send("Hello!");
+
+const db ={
+    courses: [
+        {id:1, title:"frontend"},
+        {id:2, title:"backend"}
+    ]
+} ;
+
+
+app.get("/courses", (req:Request,res:Response)=>{
+
+    let foundedCourses = db.courses;
+
+    if (req.query.title){
+        foundedCourses=foundedCourses.filter((course=> course.title.indexOf(req.query.title as string)>-1));
+    };
+
+    res.json(foundedCourses);
 })
+
+app.get("/courses/:id", (req:Request,res:Response)=>{
+   const  id = +req.params.id;
+   const foundedCourse = db.courses.find(course => course.id === id);
+
+    if (!foundedCourse){
+        res.sendStatus(404);
+        return
+    }
+    res.json(foundedCourse);
+})
+
+app.post("/courses", (req:Request,res:Response)=>{
+    const id = +new Date();
+    const title = req.body.title;
+
+    if (!title){
+        res.sendStatus(400);
+        return
+    }
+    const newCourse = {
+        id: id,
+        title:title
+    };
+    db.courses.push(newCourse);
+    res.status(201).json(newCourse);
+})
+
+app.delete("/courses/:id", (req:Request,res:Response)=>{
+    const  id = +req.params.id;
+    const courseIndex = db.courses.findIndex(course => course.id === id);
+    console.log(courseIndex)
+    if (courseIndex<=-1){
+        res.sendStatus(404);
+        return
+    }
+
+    db.courses.splice(courseIndex,1)
+
+    res.sendStatus(204);
+})
+
 app.listen(port, ()=>{
     console.log(`app start on port ${port}`);
     console.log(`open in browser http://localhost:${port}`);
